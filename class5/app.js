618c9b8241df94b70f4edc71ce4644d9ba3293ce
Muskaankamran <muskaankamran494@gmail.com>
2026-02-13 17:48:45 +0500

    search bar

--- class5/app.js
+++ class5/app.js
@@ -1,11 +1,49 @@
 import supabase from "./supabase.js";
 var cardBg;
 var editId;
+async function searchPosts() {
+  let searchValue = document.getElementById("searchValue").value;
+  var posts = document.getElementById("posts");
+  posts.innerHTML = " ";
+  try {
+    let query = supabase
+      .from("post")
+      .select("*")
+      .order("id", { ascending: false });
+    if (searchValue.trim()) {
+      query = query.ilike("title", `%${searchValue}%`);
+    }
+
+    const { data, error } = await query;
+    console.log(data);
+    data.forEach((post) => {
+      var posts = document.getElementById("posts");
+      posts.innerHTML += `
+                    <div class="card m-2">
+                      <div class="card-header">@Post ${post.id}</div>
+                      <div style="background-image: url(${post.img_url});"  class="card-body">
+                        <h5 class="card-title">${post.title}</h5>
+                        <p class="card-text">${post.description}</p>
+                      </div>
+                      <div class="ms-auto m-2">
+                          <button onclick="editPost(event, ${post.id})" class="btn btn-success">Edit</button>
+                          <button onclick="deletePost(event, ${post.id})" class="btn btn-danger">Delete</button>
+                       </div>
+                    </div>
+                    `;
+    });
+  } catch (error) {
+    console.log(error, "Error in sode");
+  }
+}
 
 //Get (fetch) all posts from supabase and display on page load
 window.addEventListener("DOMContentLoaded", async () => {
   try {
-    const { data, error } = await supabase.from("post").select("*").order('id', { ascending: false });
+    const { data, error } = await supabase
+      .from("post")
+      .select("*")
+      .order("id", { ascending: false });
     console.log(data);
     data.forEach((post) => {
       var posts = document.getElementById("posts");
@@ -76,9 +114,9 @@ async function post() {
         const { data, error } = await supabase
           .from("post")
           .update({ title, description, img_url: cardBg })
-          .eq("id",editId)
+          .eq("id", editId)
           .select("*");
-          editId = null
+        editId = null;
       } else {
         const { data, error } = await supabase
           .from("post")
@@ -88,7 +126,6 @@ async function post() {
         if (error) console.log("Post error: ", error);
       }
 
-      
       document.getElementById("title").value = "";
       document.getElementById("description").value = "";
     } catch (error) {
