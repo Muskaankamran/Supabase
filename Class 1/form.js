42798a2b86c892377bc4e958288e74da29937390
Muskaankamran <muskaankamran494@gmail.com>
2026-02-19 14:59:18 +0500

    update form

---
+++ Class 1/form.js
@@ -0,0 +1,50 @@
+import supabase from "./supabase.js";
+
+function showSignup() {
+    document.getElementById("signUp").style.display = "flex";
+    document.getElementById("login").style.display = "none";
+    document.getElementById("btns2").style.backgroundColor = "white";
+    document.getElementById("btns1").style.backgroundColor = "#cdb4db";
+}
+
+function showLogin() {
+    document.getElementById("signUp").style.display = "none";
+    document.getElementById("login").style.display = "flex";
+    document.getElementById("btns1").style.backgroundColor = "white";
+    document.getElementById("btns2").style.backgroundColor = "#cdb4db";
+}
+
+window.showLogin = showLogin;
+window.showSignup = showSignup;
+
+async function login(event) {
+    event.preventDefault();
+    let email = document.getElementById("loginEmail").value;
+    let password = document.getElementById("loginPassword").value;
+
+    const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+
+    if (error) {
+        alert(error.message);
+    } else {
+        alert("Login successful!");
+        window.location.href = "dashboard.html";
+    }
+}
+window.login = login;
+
+async function signup(event) {
+    event.preventDefault();
+    let email = document.getElementById("signupEmail").value;
+    let password = document.getElementById("signupPassword").value;
+
+    const { data, error } = await supabase.auth.signUp({ email, password });
+
+    if (error) {
+        alert(error.message);
+    } else {
+        alert("Signup successful! Redirecting to dashboard...");
+        window.location.href = "dashboard.html";
+    }
+}
+window.signup = signup;
--- class5/supabase.js
+++ class5/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-let SUPABASE_URL = "https://ingjdyteycutgcfwghxt.supabase.co";
-let SUPABASE_ANON_KEY = "sb_publishable_tET2uORt-m94WhD8qnRlKA_DFyMF3LO";
+let SUPABASE_URL = "https://xzwwaqbuyodnxkvewprv.supabase.co";
+let SUPABASE_ANON_KEY = "sb_publishable_p2iGkUXSOQWUuNt5_J1-mw_SOYtmGFa";
 var supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export default supabase
